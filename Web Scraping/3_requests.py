6b429ea31062a7a1142e82950fffe2ed742d6989
Ryu hyeon seok <gustjr1058@naver.com>
2023-09-18 21:07:57 +0900

    first

---
+++ Web Scraping/3_requests.py
@@ -0,0 +1,18 @@
+import requests
+res = requests.get("http://naver.com") # url을 받아옴
+print("응답코드 :", res.status_code) # 200 이면 정상
+
+if res.status_code == requests.codes.ok: # status_code가 200이면
+    print("정상입니다.")
+else:
+    print("문제가 생겼습니다. [에러코드 ", res.status_code, "]")
+
+res.raise_for_status() # 올바른 HTML을 가져왔다면 정상작동, 잘못된 HTML이라면 에러
+print("웹 스크래핑을 진행합니다.")
+
+# 결론 # 세트로 쓰기
+res = requests.get("http://google.com")
+res.raise_for_status()
+
+print(len(res.text))
+print(res.text)
